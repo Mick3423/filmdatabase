da693edd1b75756aac0c1f389ac75f72d1146fca
mich1 <Mich.1999@hotmail.com>
2023-09-27 09:49:01 +0200

    index.html lavet om til index.php og forside lavet

--- index.php
+++ index.php
@@ -1,8 +1,124 @@
-<?php
-require "settings/init.php";
+<!DOCTYPE html>
+<html lang="da">
+<head>
+	<meta charset="utf-8">
+	
+	<title>filmdatabase forside </title>
+	
+	<meta name="robots" content="All">
+	<meta name="author" content="Udgiver">
+	<meta name="copyright" content="Information om copyright">
+	
+	<link href="css/bootstrap.css" rel="stylesheet" type="text/css">
+	<link href="css/styles.css" rel="stylesheet" type="text/css">
+	
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+</head>
 
-$blogs = $db->sql("SELECT * FROM blogs");
+<body>
 
-foreach ($blogs as $blog){
-    echo $blog->blogName . "<br>";
-}
+<nav class="navbar navbar-expand-lg bg-light">
+	<div class="container-fluid">
+		<a class="navbar-brand" href="#">filmdatabase</a>
+		<button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+			<span class="navbar-toggler-icon"></span>
+		</button>
+		<div class="collapse navbar-collapse" id="navbarSupportedContent">
+			<ul class="navbar-nav me-auto mb-2 mb-lg-0">
+				<li class="nav-item">
+					<a class="nav-link active" aria-current="page" href="#">forside</a>
+				</li>
+				<li class="nav-item">
+					<a class="nav-link active" aria-current="page" href="#">liste</a>
+				</li>
+				<li class="nav-item">
+					<a class="nav-link active" aria-current="page" href="#">se mere</a>
+				</li>
+
+
+			</ul>
+
+		</div>
+	</div>
+</nav>
+
+
+
+<div class="container pb-5 my-5 bg-light">
+	<div class="row p-4 pb-0 pe-lg-0 pt-lg-5 align-items-center rounded-3 border shadow-lg">
+		<div class="col-lg-7 p-3 p-lg-5 pt-lg-3">
+			<h3 class="display-4 fw-bold lh-1">filmdatabase</h3>
+			<p class="lead"> Lorem ipsum dolor sit amet, consectetur adipisicing elit. Alias fugiat illum numquam perferendis ratione! Accusantium amet commodi ipsa natus neque optio soluta tempora, voluptatibus. Animi aperiam architecto et praesentium sint! Lorem ipsum dolor sit amet, consectetur adipisicing elit. Alias consequuntur doloremque ducimus, et facere in ipsa nemo odio officiis perspiciatis provident quis quo reiciendis saepe sit, tempora tenetur voluptate? Asperiores.</p>
+
+		</div>
+		<div class="col-lg-4 offset-lg-1 p-0 overflow-hidden ">
+			<img class="rounded-circle" src="" alt="" >
+		</div>
+	</div>
+</div>
+
+
+
+
+<div class="container">
+<div class="row">
+
+     <div class="card col-12 col-md-6 col-lg-4 m">
+
+                     <div class="card-body">
+                         <h5 class="card-title text-uppercase">velkommen</h5>
+                         <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Animi asperiores commodi ex inventore modi odit, ratione. Aspernatur aut ducimus eius, expedita laboriosam laudantium libero placeat possimus quisquam tempore temporibus voluptas?</p>
+
+
+                     </div>
+	 	</div>
+
+	     <div class="card col-12 col-md-6 col-lg-4">
+
+	                     <div class="card-body">
+	                         <h5 class="card-title text-uppercase">liste</h5>
+	                         <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ab accusantium consequuntur corporis deleniti eum excepturi impedit, modi nemo pariatur, placeat quasi ratione reiciendis repudiandae similique sit tenetur vero voluptatum? Maiores.</p>
+	                         <a href="" class="btn btn-primary">se listen her</a>
+
+	                     </div>
+		 </div>
+
+	     <div class="card col-12 col-md-6 col-lg-4">
+
+	                     <div class="card-body">
+	                         <h5 class="card-title text-uppercase">se mere</h5>
+	                         <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Atque ex explicabo iusto quos saepe. Adipisci iste perspiciatis quas sed vel vero voluptates. Adipisci aperiam atque distinctio ducimus fugit harum, quasi?</p>
+	                         <a href="" class="btn btn-primary">se mere her</a>
+
+	                     </div>
+		 </div>
+
+</div>
+
+</div>
+
+<footer>
+	<div class="container-fluid text-dark text-center bg-light mt-5">
+
+		<p class="display-4 mb-1 text-uppercase">info</p>
+		<ul>
+			<li> Lorem ipsum dolor </li>
+			<li>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ad, dicta!</li>
+			<li>Lorem ipsum dolor sit amet, consectetur adipisicing.</li>
+			<li>Lorem ipsum dolor sit amet.</li>
+			<li>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</li>
+
+
+		</ul>
+		<h5>Kontaktoplysninger
+		</h5>
+		<p> mail: Mail@filmdatabase.dk </p>
+		<p> tlf nummer: xxxxxxx</p>
+
+
+	</div>
+</footer>
+
+<script src="node_modules/bootstrap/dist/js/bootstrap.bundle.min.js"></script>
+</body>
+</html>
