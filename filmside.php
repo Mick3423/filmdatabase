431f49997b1dd13c9bfc7a7468f133042610d3e3
mich1 <Mich.1999@hotmail.com>
2023-10-03 12:33:24 +0200

    filmside.php lavet

--- filmside.php
+++ filmside.php
@@ -1,33 +1,172 @@
-<!-- Instruktion til webbrowser om at vi kører HTML5 -->
 <!DOCTYPE html>
-
-<!-- html starter og slutter hele dokumentet / lang=da: Fortæller siden er på dansk -->
 <html lang="da">
-
-<!-- I <head> har man opsætning - det ser brugeren ikke, men det fortæller noget om siden -->
 <head>
-    <!-- Sætter tegnsætning til utf-8 som bl.a. tillader danske bogstaver -->
     <meta charset="utf-8">
 
-    <!-- Titel som ses oppe i browserens tab mv. -->
-    <title>Sigende titel</title>
+    <title>filmdatabase forside </title>
 
-    <!-- Metatags der fortæller at søgemaskiner er velkomne, hvem der udgiver siden og copyright information -->
     <meta name="robots" content="All">
     <meta name="author" content="Udgiver">
     <meta name="copyright" content="Information om copyright">
 
-    <!-- Sikrer man kan benytte CSS ved at tilkoble en CSS fil -->
+    <link href="css/bootstrap.css" rel="stylesheet" type="text/css">
     <link href="css/styles.css" rel="stylesheet" type="text/css">
 
-    <!-- Sikrer den vises korrekt på mobil, tablet mv. ved at tage ift. skærmstørrelse - bliver brugt til responsive websider -->
     <meta name="viewport" content="width=device-width, initial-scale=1">
 </head>
 
-<!-- i <body> har man alt indhold på siden som brugeren kan se -->
 <body>
 
-<!-- Her skal sidens indhold ligge -->
+<nav class="navbar navbar-expand-lg bg-light">
+    <div class="container-fluid">
+        <a class="navbar-brand" href="#">filmdatabase</a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                <li class="nav-item">
+                    <a class="nav-link active" aria-current="page" href="#">forside</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link active" aria-current="page" href="#">liste</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link active" aria-current="page" href="#">filmside</a>
+                </li>
 
+
+            </ul>
+
+        </div>
+    </div>
+</nav>
+
+
+
+<div class="container pb-5 my-5 bg-light">
+    <div class="row p-4 pb-0 pe-lg-0 pt-lg-5 align-items-center rounded-3 border shadow-lg">
+        <div class="col-lg-7 p-3 p-lg-5 pt-lg-3">
+            <h3 class="display-4 fw-bold lh-1">John Wick</h3>
+            <p class="lead"> John Wick is a retired hitman who returns to the criminal underworld to settle a debt. When his former employer's son, Santino, orders his men to take out John's beloved puppy, an unexpected and deadly altercation ensues, leaving his beloved pup dead. Fueled by vengeance and a drive for justice, John embarks on a brutal rampage to take down Santino and his gang </p>
+
+        </div>
+        <div class="col-lg-4 offset-lg-1 p-0 overflow-hidden ">
+            <img  src="uploads/John_Wick_TeaserPoster.jpg" alt="john wick plakat" >
+        </div>
+    </div>
+</div>
+
+
+
+
+<div class="container">
+    <div class="row">
+
+        <div class="card col-12 col-md-6 col-lg-4 m">
+
+            <div class="card-body">
+                <h5 class="card-title text-uppercase">pris</h5>
+                <p class="card-text"> prisen på at leje eller købe John wick er 40 kr</p>
+
+
+            </div>
+        </div>
+
+        <div class="card col-12 col-md-6 col-lg-4">
+
+            <div class="card-body">
+                <h5 class="card-title text-uppercase">filmens længde</h5>
+                <p class="card-text">filmen har en længde på 101 minutter </p>
+
+
+            </div>
+        </div>
+
+        <div class="card col-12 col-md-6 col-lg-4">
+
+            <div class="card-body">
+                <h5 class="card-title text-uppercase">filmengenre </h5>
+                <p class="card-text"> action/crime/thriller</p>
+
+
+            </div>
+        </div>
+
+    </div>
+
+</div>
+
+<div class="container mt-5">
+    <div class="row">
+
+        <div class="card col-12 col-md-6 col-lg-4 m">
+
+            <div class="card-body">
+                <h5 class="card-title text-uppercase">instruktør</h5>
+                <p class="card-text"> filmens instruktør er Chad Stahelski</p>
+
+
+            </div>
+        </div>
+
+        <div class="card col-12 col-md-6 col-lg-4">
+
+            <div class="card-body">
+                <h5 class="card-title text-uppercase">Medvirkende</h5>
+                <p class="card-text"> >Keanu Reeves, Michael Nyqvist, Alfie Allen, Bridget Moynahan, Dean Winthers, Ian McShane, John Leguzamo, Willem Dafoe </p>
+
+
+            </div>
+        </div>
+
+        <div class="card col-12 col-md-6 col-lg-4">
+
+            <div class="card-body">
+                <h5 class="card-title text-uppercase">filmens budget</h5>
+                <p class="card-text">filmen havde et Budget på 30 millioner dollars</p>
+
+
+            </div>
+        </div>
+        <div class="card col-12 col-md-6 col-lg-4 justify-content-center mt-3 ">
+
+            <div class="card-body">
+                <h5 class="card-title text-uppercase">filmens oprettelse på databasen</h5>
+                <p class="card-text"> filmen blev oprettet på databasen 2023-10-03</p>
+
+
+            </div>
+        </div>
+
+    </div>
+
+</div>
+
+
+
+<footer>
+    <div class="container-fluid text-dark text-center bg-light mt-5">
+
+        <p class="display-4 mb-1 text-uppercase">info</p>
+        <ul>
+            <li> Lorem ipsum dolor </li>
+            <li>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ad, dicta!</li>
+            <li>Lorem ipsum dolor sit amet, consectetur adipisicing.</li>
+            <li>Lorem ipsum dolor sit amet.</li>
+            <li>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</li>
+
+
+        </ul>
+        <h5>Kontaktoplysninger
+        </h5>
+        <p> mail: Mail@filmdatabase.dk </p>
+        <p> tlf nummer: xxxxxxx</p>
+
+
+    </div>
+</footer>
+
+<script src="node_modules/bootstrap/dist/js/bootstrap.bundle.min.js"></script>
 </body>
 </html>
